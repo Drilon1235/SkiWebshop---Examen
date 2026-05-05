40b68b2ba46dbb6d0446d51a5d9f2433e5d7301b
Drilon Zenuni <drilon.zenuni@fluxus.nu>
2026-05-05 14:59:47 +0200

    s

---
+++ WebShopLogica/Managers/AanmeldenManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopLogica.Managers
+{
+    public static class AanmeldenManager
+    {
+        public static string Connectionstring { get; set; }
+
+        public static bool Aanmelden(string gebruikersnaam, string wachtwoord)
+        {
+            using (SqlConnection connection = new SqlConnection(Connectionstring))
+            {
+                connection.Open();
+                string query = "SELECT Gebruikersnaam, Wachtwoord FROM Gebruikers WHERE Gebruikersnaam = @gebruikersnaam AND Wachtwoord = @wachtwoord";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@gebruikersnaam", gebruikersnaam);
+                    command.Parameters.AddWithValue("@wachtwoord", wachtwoord);
+                    int count = (int)command.ExecuteScalar();
+                    return count > 0;
+                }
+            }
+        }
+    }
+}
---
+++ WebShopLogica/Objects/AanmeldenObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopLogica.Objects
+{
+    public static class AanmeldenObject
+    {
+        public static string Gebruikersnaam { get; set; }
+        public static string Wachtwoord { get; set; }
+    }
+}
